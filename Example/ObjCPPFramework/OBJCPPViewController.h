20f809806a9fee3f74f0368e8d3a4baa44b4bc80
Justin Goh <justin.goh@thoughtworks.com>
2019-12-07 23:46:39 +0800

    convert objC to objC++ framework by renaming file to .mm, removing @imports and converting to #import

--- Example/ObjCPPFramework/OBJCPPViewController.h
+++ Example/ObjCPPFramework/OBJCPPViewController.h
@@ -1,12 +1,4 @@
-//
-//  OBJCPPViewController.h
-//  ObjCPPFramework
-//
-//  Created by jgch-tw on 12/07/2019.
-//  Copyright (c) 2019 jgch-tw. All rights reserved.
-//
-
-@import UIKit;
+#import <UIKit/UIKit.h>
 
 @interface OBJCPPViewController : UIViewController
 
